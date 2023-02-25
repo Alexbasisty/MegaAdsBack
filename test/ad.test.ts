796e625c01d411f15651fca2b10b09eed79a1652
Alex Basystyi <alexbasisty@gmail.com>
2023-02-25 15:43:32 +0100

    added searching and listing of ads

--- test/ad.test.ts
+++ test/ad.test.ts
@@ -1,4 +1,10 @@
-import { AdRecord } from "../record/ad.record"
+import { AdRecord } from "../record/ad.record";
+import { AdEntity } from "../types";
+import { pool } from "../utils/db";
+
+afterAll(async () => {
+    await pool.end();
+});
 
 test('AdRecord returns data from database from one entry',async () => {
     const ad = await AdRecord.getOne('899572e7-b50c-11ed-ae22-f894c2e5c649');
@@ -11,4 +17,31 @@ test('AdRecord returns null from DB for unexisting entry.',async () => {
     const ad = await AdRecord.getOne('abc');
 
     expect(ad).toBeNull();    
+});
+
+test('AdRecord.findAll returns array of found entries',async () => {
+    const ads = await AdRecord.findAll('');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+});
+
+test('AdRecord.findAll returns array of found entries when searching for "a"',async () => {
+    const ads = await AdRecord.findAll('we');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+});
+
+test('AdRecord.findAll returns array of found entries when searching for smth that does not exist',async () => {
+    const ads = await AdRecord.findAll('------------------');
+
+    expect(ads).toEqual([]);
+});
+
+test('AdRecord.findAll returns smaller amount of data',async () => {
+    const ads = await AdRecord.findAll('');
+
+    expect((ads[0] as AdEntity).price).toBeUndefined();
+    expect((ads[0] as AdEntity).description).toBeUndefined();
 });
